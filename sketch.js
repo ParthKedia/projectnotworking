b818c71c4049831e72cd25ce6bc7debb90fa84bc
ParthKedia <75347391+ParthKedia@users.noreply.github.com>
2021-02-02 16:26:42 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	stoneObj=new stone(225,410,30);
+	
+	launcherObject = new slingshot(stone.body,{x:225, y:410});
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  stoneObj.display();
+
+  groundObject.display();
+}
+
+function mouseDragged(){
+	Matter.Body.setPosition(stone.body,{x:mouseX , y:mouseY});
+}
+
+function mouseReleased(){
+	launcherObject.mouseR();
+}
